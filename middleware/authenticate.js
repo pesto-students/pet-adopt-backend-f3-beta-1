069049f9545a68329c5840f455fd837012118cbb
atul <atul.example.com>
2022-01-12 20:31:47 +0530

    authenticate middle solved

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,26 +1,25 @@
-const jwt = require("jsonwebtoken");
-const User = require("../model/userSchema");
+const jwt = require('jsonwebtoken');
+const User = require('../model/userSchema')
 
-const authenticate = async (req, res, next) => {
-  try {
-    const token = req.cookies.jwtoken;
-    const verifyToken = jwt.verify(token, process.env.SECRET_KEY);
-    const rootUser = await User.findOne({
-      _id: verifyToken._id,
-      "tokens.token": token,
-    });
-    if (rootUser) {
-      req.token = token;
-      req.rootUser = rootUser;
-      req.userID = rootUser._id;
-      next();
-    } else {
-      throw new Error("User not found");
+const authenticate = async (req , res , next) => {
+    try {
+        const token = req.cookies.jwtoken;
+        const verifyToken = jwt.verify(token, process.env.SECRET_KEY)
+
+        const rootUser = await User.findOne({_id: verifyToken._id, "tokens.token":token });
+
+        if(!rootUser) {throw new Error("User Not Found")};
+
+        req.token = token;
+        req.rootUser = rootUser;
+        req.userID = rootUser._id;
+
+        next();
     }
-  } catch (error) {
-    res.status(401).send("Unauthorized: token not provided");
-    console.log(error);
-  }
-};
+    catch (err) {
+        res.status(404).send('Unauthorized: No token found')
+        console.log(err);
+    }
+}
 
 module.exports = authenticate;
