85094bf6b8e98285801a0b226edda78a610b19ce
atul <atul.example.com>
2022-01-17 15:21:49 +0530

    for cors

--- app.js
+++ app.js
@@ -6,18 +6,9 @@ const app = express();
 dotenv.config({path: './config.env'});
 require('./db/conn');
 
-app.use((req, res, next) => {
-    if (req.method === "OPTIONS") {
-      res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-      return res.status(200).json({});
-    }
-    next();
-  });
 app.use(express.json());
 app.use(cookieParser());
 app.use(require('./router/auth'));
-
-const User = require('./model/userSchema');
 
 const PORT = process.env.PORT || 5000;
 
