a5dadc0faa0751837b8ac51247feef08b7f3e11b
atul <atul.example.com>
2022-01-20 20:57:28 +0530

    authenticate done

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -5,7 +5,7 @@ const authenticate = async (req , res , next) => {
     try {
         const token = req.cookies.jwtoken;
         const verifyToken = jwt.verify(token, process.env.SECRET_KEY)
-        console.log(token);
+
         const rootUser = await User.findOne({_id: verifyToken._id, "tokens.token":token });
 
         if(!rootUser) {throw new Error("User Not Found")};
